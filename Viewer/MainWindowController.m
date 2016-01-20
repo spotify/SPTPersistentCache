37b30c034649314c8bba62ca31faafa91b20cf6d
Dan Fleming <dflems@spotify.com>
2016-01-19 19:37:26 -0500

    fix warning in viewer

--- Viewer/MainWindowController.m
+++ Viewer/MainWindowController.m
@@ -131,7 +131,7 @@
     NSString *fullFilePath = [self.cacheFiles objectAtIndex:idx];
     NSData *rawData = [NSData dataWithContentsOfFile:fullFilePath];
 
-    SPTPersistentRecordHeaderType *h = pdc_GetHeaderFromData([rawData bytes], [rawData length]);
+    SPTPersistentRecordHeaderType *h = pdc_GetHeaderFromData(__DECONST(void*, [rawData bytes]), [rawData length]);
 
     if (h == NULL) {
         // TODO: error
