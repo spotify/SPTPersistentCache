d23418783b6ec2073a8d58f6b12abb57996609f3
Aron Cedercrantz <cedercra@spotify.com>
2016-02-05 23:28:18 +0100

    Add ci scripts

---
+++ ci/after_success.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+source "./ci/lib/travis_helpers.sh"
+
+set -euo pipefail
+
+travis_fold_open "Slather" "Publishing test coverage data…"
+bundle exec slather coverage \
+    --input-format profdata \
+    --scheme "${SCHEME}" \
+    "${PROJECT}"
+travis_fold_close "Slather"
