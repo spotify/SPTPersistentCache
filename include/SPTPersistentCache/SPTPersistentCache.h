ee65d8ed38d45098872d7251f4a81ed8d909a4a3
Aron Cedercrantz <aron@cedercrantz.se>
2016-02-21 01:30:44 +0100

    Move framework version constants to top of file
    - It should be up there :)

--- include/SPTPersistentCache/SPTPersistentCache.h
+++ include/SPTPersistentCache/SPTPersistentCache.h
@@ -20,6 +20,17 @@
  */
 #import <Foundation/Foundation.h>
 
+#ifndef SPT_BUILDING_FRAMEWORK
+#define SPT_BUILDING_FRAMEWORK 0
+#endif
+#if SPT_BUILDING_FRAMEWORK
+//! Project version number for SPTDataLoader.
+FOUNDATION_EXPORT double SPTDataLoaderVersionNumber;
+
+//! Project version string for SPTDataLoader.
+FOUNDATION_EXPORT const unsigned char SPTDataLoaderVersionString[];
+#endif // SPT_BUILDING_FRAMEWORK
+
 #import <SPTPersistentCache/SPTPersistentCacheOptions.h>
 #import <SPTPersistentCache/SPTPersistentCacheHeader.h>
 #import <SPTPersistentCache/SPTPersistentCacheRecord.h>
@@ -64,16 +75,6 @@ typedef NS_ENUM(NSInteger, SPTPersistentCacheLoadingError) {
     SPTPersistentCacheLoadingErrorInternalInconsistency
 };
 
-#ifndef SPT_BUILDING_FRAMEWORK
-#define SPT_BUILDING_FRAMEWORK 0
-#endif
-#if SPT_BUILDING_FRAMEWORK
-//! Project version number for SPTDataLoader.
-FOUNDATION_EXPORT double SPTDataLoaderVersionNumber;
-
-//! Project version string for SPTDataLoader.
-FOUNDATION_EXPORT const unsigned char SPTDataLoaderVersionString[];
-#endif // SPT_BUILDING_FRAMEWORK
 /**
  * The error domain for errors produced by the persistent cache.
  */
