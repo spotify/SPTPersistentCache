4bcc5c506dd81cfbb51a7152c903ddfa3a17d14b
Dan Fleming <dflems@spotify.com>
2019-01-03 15:34:17 -0500

    It's 2019 🎉

--- Sources/SPTPersistentCachePosixWrapper.m
+++ Sources/SPTPersistentCachePosixWrapper.m
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2018 Spotify AB.
+ Copyright (c) 2019 Spotify AB.
 
  Licensed to the Apache Software Foundation (ASF) under one
  or more contributor license agreements.  See the NOTICE file
