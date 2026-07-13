69655241f72f3dc2d3a09e550a1b391e9366ec4e
Dan Fleming <dflems@spotify.com>
2026-07-13 13:33:42 -0400

    Group CI

--- ci/run.sh
+++ ci/run.sh
@@ -1,59 +1,61 @@
 #!/bin/bash
+set -e
 
-heading() {
-  echo ""
-  echo -e "\033[0;35m** ${*} **\033[0m"
-  echo ""
-}
+if [[ -n "$TRAVIS_BUILD_ID" || -n "$GITHUB_WORKFLOW" ]]; then
+  export IS_CI=1
+fi
 
-fail() {
-  >&2 echo "error: $@"
-  exit 1
+DERIVED_DATA_COMMON="build/DerivedData/common"
+DERIVED_DATA_TEST="build/DerivedData/test"
+
+groupify() {
+  echo "::group::$1"
+  shift
+  (
+      trap 'echo "::endgroup::"' EXIT
+      "$@" 
+  )
 }
 
 xcb() {
-  LOG="$1"
-  heading "$LOG"
-  shift
-  export NSUnbufferedIO=YES
-  set -o pipefail && xcodebuild \
+  set -o pipefail && NSUnbufferedIO=YES xcodebuild \
     -UseSanitizedBuildSystemEnvironment=YES \
     CODE_SIGNING_REQUIRED=NO CODE_SIGN_IDENTITY= \
-    "$@" | xcbeautify || fail "$LOG failed"
+    "$@" | xcbeautify
 }
 
 build_target() {
-  xcb "Build SPTPersistentCache [$1]" build \
+  groupify "Build for $1" xcb build \
     -scheme SPTPersistentCache \
     -destination "$1" \
     -configuration Release \
     -derivedDataPath "$DERIVED_DATA_COMMON"
 }
 
-DERIVED_DATA_COMMON="build/DerivedData/common"
-DERIVED_DATA_TEST="build/DerivedData/test"
-
-if [[ -n "$TRAVIS_BUILD_ID" || -n "$GITHUB_WORKFLOW" ]]; then
-  heading "Installing Tools"
+install_tools() {
   gem install cocoapods
   brew install xcbeautify
-  export IS_CI=1
+}
+
+validate_license_conformance() {
+  git ls-files | egrep "\\.(h|m|mm)$" | \
+    xargs ci/validate_license_conformance.sh ci/expected_license_header.txt
+}
+
+if [[ "$IS_CI" == "1" ]]; then
+  groupify "Install Tools" install_tools
 fi
 
-heading "Linting Podspec"
-pod spec lint SPTPersistentCache.podspec --quick || \
-  fail "Podspec lint failed"
+groupify "Lint Podspec" \
+  pod spec lint SPTPersistentCache.podspec --quick
 
-heading "Validating License Conformance"
-git ls-files | egrep "\\.(h|m|mm)$" | \
-  xargs ci/validate_license_conformance.sh ci/expected_license_header.txt || \
-  fail "License Validation Failed"
+groupify "Validate License Conformance" validate_license_conformance
 
 #
 # RUN TESTS (spm + macos)
 #
 
-swift test || fail "spm tests failed"
+groupify "macOS SPM tests" swift test
 
 #
 # BUILD LIBRARIES (xcodebuild)
@@ -71,29 +73,39 @@ build_target "generic/platform=watchOS Simulator"
 # RUN TESTS (Xcode)
 #
 
-xcb "Run tests for macOS" test \
-  -scheme "SPTPersistentCache" \
-  -enableCodeCoverage YES \
-  -destination "platform=macOS" \
-  -derivedDataPath "$DERIVED_DATA_TEST/macos"
+test_macos() {
+  xcb test \
+    -scheme "SPTPersistentCache" \
+    -enableCodeCoverage YES \
+    -destination "platform=macOS" \
+    -derivedDataPath "$DERIVED_DATA_TEST/macos"
+}
 
-LATEST_IOS_RUNTIME=`xcrun simctl list runtimes | egrep "^iOS" | sort | tail -n 1 | awk '{print $NF}'`
-IOS_UDID=`xcrun simctl create sptpc-ios-tester com.apple.CoreSimulator.SimDeviceType.iPhone-13 "$LATEST_IOS_RUNTIME"`
+test_ios() {
+  LATEST_IOS_RUNTIME=`xcrun simctl list runtimes | egrep "^iOS" | sort | tail -n 1 | awk '{print $NF}'`
+  IOS_UDID=`xcrun simctl create sptpc-ios-tester com.apple.CoreSimulator.SimDeviceType.iPhone-13 "$LATEST_IOS_RUNTIME"`
 
-xcb "Run tests for iOS" test \
-  -scheme "SPTPersistentCache" \
-  -enableCodeCoverage YES \
-  -destination "platform=iOS Simulator,id=$IOS_UDID" \
-  -derivedDataPath "$DERIVED_DATA_TEST/ios"
+  xcb test \
+    -scheme "SPTPersistentCache" \
+    -enableCodeCoverage YES \
+    -destination "platform=iOS Simulator,id=$IOS_UDID" \
+    -derivedDataPath "$DERIVED_DATA_TEST/ios"
+}
 
-LATEST_TVOS_RUNTIME=`xcrun simctl list runtimes | egrep "^tvOS" | sort | tail -n 1 | awk '{print $NF}'`
-TVOS_UDID=`xcrun simctl create sptpc-tvos-tester com.apple.CoreSimulator.SimDeviceType.Apple-TV-1080p "$LATEST_TVOS_RUNTIME"`
+test_tvos() {
+  LATEST_TVOS_RUNTIME=`xcrun simctl list runtimes | egrep "^tvOS" | sort | tail -n 1 | awk '{print $NF}'`
+  TVOS_UDID=`xcrun simctl create sptpc-tvos-tester com.apple.CoreSimulator.SimDeviceType.Apple-TV-1080p "$LATEST_TVOS_RUNTIME"`
 
-xcb "Run tests for tvOS" test \
-  -scheme "SPTPersistentCache" \
-  -enableCodeCoverage YES \
-  -destination "platform=tvOS Simulator,id=$TVOS_UDID" \
-  -derivedDataPath "$DERIVED_DATA_TEST/tvos"
+  xcb test \
+    -scheme "SPTPersistentCache" \
+    -enableCodeCoverage YES \
+    -destination "platform=tvOS Simulator,id=$TVOS_UDID" \
+    -derivedDataPath "$DERIVED_DATA_TEST/tvos"
+}
+
+groupify "Run tests for macOS" test_macos
+groupify "Run tests for iOS" test_ios
+groupify "Run tests for tvOS" test_tvos
 
 #
 # CODECOV
@@ -117,11 +129,13 @@ if [[ -n "$GITHUB_WORKFLOW" ]]; then
   export VCS_SLUG="$GITHUB_REPOSITORY"
 fi
 
-curl -sfL https://codecov.io/bash > build/codecov.sh
-chmod +x build/codecov.sh
-[[ "$IS_CI" == "1" ]] || CODECOV_EXTRA="-d"
+coverage_prepare() {
+  curl -sfL https://codecov.io/bash > build/codecov.sh
+  chmod +x build/codecov.sh
+}
 
 coverage_report() {
+  [[ "$IS_CI" == "1" ]] || CODECOV_EXTRA="-d"
   build/codecov.sh -F "$1" -D "$DERIVED_DATA_TEST/$1" -X xcodellvm $CODECOV_EXTRA
   if [[ "$IS_CI" == "1" ]]; then
     # clean up coverage files so they don't leak into the next processing run
@@ -133,6 +147,7 @@ coverage_report() {
   fi
 }
 
-coverage_report macos
-coverage_report tvos
-coverage_report ios
+groupify "Coverage preparation" coverage_prepare
+groupify "Coverage for macOS" coverage_report macos
+groupify "Coverage for tvOS" coverage_report tvos
+groupify "Coverage for iOS" coverage_report ios
